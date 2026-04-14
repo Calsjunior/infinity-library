749b32f8aeab10847da51c46b2bcc5f2bacfe81c
cal <sakphea05@gmail.com>
2026-04-14 15:41:05 +0700

    feat: add script for adding book to library
    This script was made in a different project folder so I just paste it
    here

---
+++ js/main.js
@@ -0,0 +1,78 @@
+const myLibrary = [];
+
+function Book(title, author, pages, read) {
+    if (!new.target) {
+        throw Error("You must use the new operator to call the constructor");
+    }
+
+    this.id = crypto.randomUUID();
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+}
+
+Book.prototype.toggleReadStatus = function () {
+    this.read = !this.read;
+};
+
+function addBookToLibrary(book) {
+    myLibrary.push(book);
+}
+
+function removeBookFromLibrary(book) {
+    myLibrary.splice(book, 1);
+}
+
+function getBookFromInput() {
+    const form = document.querySelector("#add-book-form");
+    const bookTitle = form.elements["book-title"].value;
+    const bookAuthor = form.elements["book-author"].value;
+    const bookPages = form.elements["book-pages"].value;
+    const bookStatus = form.elements["book-status"].checked;
+    return new Book(bookTitle, bookAuthor, bookPages, bookStatus);
+}
+
+function displayBooks() {
+    const bookshelf = document.querySelector(".bookshelf");
+    bookshelf.innerHTML = "";
+    myLibrary.forEach((book) => {
+        const card = document.createElement("article");
+        card.classList.add("book__spine");
+        card.innerHTML = `
+            <h2 class="book__title">${book.title}</h2>
+            <p class="book__author">Author: ${book.author}</p>
+            <p class="book__pages">Pages: ${book.pages}</p>
+            <button class="book__button book__button--read" data-id=${book.id}>Status: ${book.read ? "Read" : "Not Yet Read"}</button>
+            <button class="book__button book__button--remove" data-id="${book.id}">Remove Book</button>
+            `;
+
+        bookshelf.appendChild(card);
+    });
+}
+
+// Add book to myLibrary when button is clicked
+const dialog = document.querySelector("#add-book-dialog");
+const form = document.querySelector("#add-book-form");
+form.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    const newBook = getBookFromInput();
+    addBookToLibrary(newBook);
+    displayBooks();
+
+    form.reset();
+    dialog.close();
+});
+
+// Remove book from myLibrary and toggle read status
+const bookshelf = document.querySelector(".bookshelf");
+bookshelf.addEventListener("click", (event) => {
+    const targetBook = myLibrary.find((book) => book.id === event.target.dataset.id);
+    if (event.target.classList.contains("book__button--remove")) {
+        removeBookFromLibrary(targetBook.id);
+    } else if (event.target.classList.contains("book__button--read")) {
+        targetBook.toggleReadStatus();
+    }
+    displayBooks();
+});
